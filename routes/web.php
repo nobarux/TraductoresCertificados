b527f8c9e02c6c79f62d60fad13dc8fd0564755c
¨Danilo¨ <daniemiav@gmail.com>
2020-08-05 14:38:48 -0400

    Algunas rutas creadas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('solicitudesRegistro', function () {
+    //return view('welcome');
+    return "Aqui van las solicitudes de admision para los usuarios interesados";
+});
+
+////---------------------------------------------//////
+Route::get('solicitudesGeneral', function () {
+    //return view('welcome');
+    return "Aqui se veran todas las solicitudes con sus respectivos estados";
+});
+
+Route::get('solicitudesSuspensas', function () {
+    //return view('welcome');
+    return "Aqui van las solicitudes de admision para los usuarios que suspendieron";
+});
+
+Route::get('reclamaciones', function () {
+    //return view('welcome');
+    return "Aqui van las solicitudes que tengan un estado reclamacion";
+});
+
+Route::get('roles', function () {
+    //return view('welcome');
+    return "Aqui van la pantalla de los roles";
+});
