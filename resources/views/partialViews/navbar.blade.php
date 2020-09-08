bdd91e61dff51fe2a4fb206649b6d2be9effd18b
¨Danilo¨ <daniemiav@gmail.com>
2020-09-08 11:14:50 -0400

    Se agrego una pestaña par ir al admin

--- resources/views/partialViews/navbar.blade.php
+++ resources/views/partialViews/navbar.blade.php
@@ -46,11 +46,14 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                  <a class="dropdown-item" href="/admin"> Administración</a> 
+                                  
+                                  <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
